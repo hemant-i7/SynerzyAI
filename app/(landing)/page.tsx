1f5ad408c9cf0ea1644580d1763db2ce98ebb39b
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-01-24 20:11:27 +0530

    login page updated

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link  from "next/link";
+
 const LandingPage = () => {
     return (
-        <div>
-<h2>Welcome</h2>
-        </div>
+      <div>
+
+      
+      <Link href="/sign-in">
+        <Button>
+          Login
+        </Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>
+          Register
+        </Button>
+      </Link>
+      </div>
       );
 }
  
