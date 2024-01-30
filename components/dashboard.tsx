c1e22950bbdf4be17217daea97f3e94b7cabc84a
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-01-30 23:33:15 +0530

    dashboard add

---
+++ components/dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,20 +3,95 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Montserrat } from "next/font/google";
-import  { cn } from "@/lib/utils"; 
+import { cn } from "@/lib/utils";
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/Conversation",
+    color: "text-pink-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-green-500",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-yellow-500",
+  },
+  {
+    label: "Music Geneartion",
+    icon: Music,
+    href: "/music",
+    color: "text-orange-500",
+  },
+
+  {
+    label: "Code Geneation",
+    icon: Code,
+    href: "/code",
+    color: "text-red-500",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+    color: "text-white-500",
+  },
+];
+
 const Sidebar = () => {
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-black text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-gray-900 text-white">
       <div className="px-3 py-2 flex-1 ">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative h-8 w-8 mr-4">
             <Image fill alt="Logo" src="/logo.png" />
           </div>
-          <h1 className={cn("text-2xl font-bold",montserrat.className)}>Synerzy AI</h1>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Synerzy AI
+          </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group 
+              flex p-3 w-full justify-start 
+              font-medium cursor-pointer
+               hover:text-white hover:bg-white/10 
+               rounded-lg transition "
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
