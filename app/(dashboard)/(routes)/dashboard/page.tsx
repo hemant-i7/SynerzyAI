03e9d207d6835a49dc18ce7b640504f63a5cd8fe
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-02-01 23:40:41 +0530

    cards update

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
-
+import Cards from "../dashboard/cards";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -11,7 +11,7 @@ export default function DashbordPage()
 {
   return (
     
-    <div>
+    <div className="bg-gray-500">
       <div className="mb-8 space-y-4">
         <h2 className={cn("text-2xl md:text-4xl font-bold text-center ", montserrat.className)}>
           Explore the Intelligence of AI{" "}
@@ -20,6 +20,7 @@ export default function DashbordPage()
           Where Intelligence Meets Synergy
         </p>
       </div>
+      <Cards />
       <div>
 
       </div>
