4e25e648a23ba9c88f430b2316483559aea440fd
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-04-18 01:53:57 +0530

    Landing page added

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -1,20 +1,5 @@
-import { useUser } from "@clerk/nextjs";
-import { useUser } from "@clerk/nextjs";
-
-
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export const UserAvatar = () => {
-  const { user } = useUser();
-
-  return (
-    <Avatar className="h-8 w-8">
-      <AvatarImage src={user?.profileImageUrl} />
-      <AvatarFallback>
-        {user?.firstName?.charAt(0)}
-        {user?.lastName?.charAt(0)}
-      </AvatarFallback>
-    </Avatar>
-  );
+  return <Avatar className="h-8 w-8"></Avatar>;
 };
